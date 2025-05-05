f1818ab065c091adc3abb51c1e9bc5b858d72864
sparrowstuff <damnagett@gmail.com>
2025-05-05 19:11:07 +0300

    form ready, footer started

--- main.js
+++ main.js
@@ -6,3 +6,28 @@ burgerBtn.addEventListener('click', function () {
 
 	mainNavTablet.classList.toggle('show-nav-menu')
 })
+
+const subscribeForm = document.querySelector('.subscription-form__form')
+
+subscribeForm.addEventListener('submit', e => {
+	e.preventDefault()
+	console.log('Форма отработала')
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+	const headerBlock = document.querySelector('.header')
+
+	const animatedBlock = document.querySelectorAll('.hidden-block')
+
+	function headerBackgroundVisible() {
+		if (window.scrollY > 100) {
+			headerBlock.style.backgroundColor = '#38535d'
+		} else {
+			headerBlock.style.backgroundColor = 'transparent'
+		}
+	}
+
+	window.addEventListener('scroll', headerBackgroundVisible)
+
+	headerBackgroundVisible()
+})
